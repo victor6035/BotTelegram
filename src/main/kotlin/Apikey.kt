542c3045157791dcd83f40e0111d6737a78aa0a9
Victor <vsaaperez@danielcastelao.org>
2021-05-09 19:54:47 +0200

    agrego eco

--- src/main/kotlin/Apikey.kt
+++
@@ -1,3 +0,0 @@
-object Apikey {
-    val miToken = "1701880343:AAHdH2zYMwCUEYCrr3eaneDHlKTL5ucvtyg"
-}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,25 @@
-import Apikey.miToken
 import com.github.kotlintelegrambot.bot
-
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.ParseMode
+import com.github.kotlintelegrambot.network.fold
 
 fun main() {
+
     val bot = bot {
-        token = Apikey.miToken
-    }
-    dispatch {
 
-        text {
-            print("Chat ID: ")
-            println(message.chat.id)
-            bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = text)
+        token =  "1776107814:AAF4_qjRVgQz0sdKa20hPy-ZX-KTlVy3Hwg"
 
+        dispatch {
 
+            text {
+
+                bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = text)
+            }
         }
     }
+
+    bot.startPolling()
 }
