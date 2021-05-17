77ee748fee779d1d66ceb0c1543120773758b782
victor6035 <72441619+victor6035@users.noreply.github.com>
2021-05-17 23:20:05 +0200

    Delete Constantes.kt

--- src/main/kotlin/Constantes.kt
+++
@@ -1,6 +0,0 @@
-object Constantes {
-
-    val miToken = "1737882380:AAEfr58ugEz4UKUQTnEair_NPi8VQTTV1ig"
-}
-//token victor"1776107814:AAF4_qjRVgQz0sdKa20hPy-ZX-KTlVy3Hwg"
-//token gabriel  "1737882380:AAEfr58ugEz4UKUQTnEair_NPi8VQTTV1ig"
