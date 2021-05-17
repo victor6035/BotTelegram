3a2fe6f7db1c38f5eae3751e12091aef4f93ed0c
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2021-05-17 23:01:42 +0200

    fin commit

--- src/main/kotlin/Encuesta.kt
+++ src/main/kotlin/Encuesta.kt
@@ -17,7 +17,7 @@ class Encuesta {
                 pollAnswer {
                     println("${pollAnswer.user.username} has selected the option ${pollAnswer.optionIds.lastOrNull()} in the poll ${pollAnswer.pollId}")
                 }
-
+                    //encuesta
                 command("prefieres") {
                     bot.sendPoll(
                         chatId = ChatId.fromId(message.chat.id),
@@ -29,7 +29,7 @@ class Encuesta {
 
                     )
                 }
-
+                //encuesta
                 command("accion") {
                     bot.sendPoll(
                         chatId = ChatId.fromId(message.chat.id),
@@ -40,7 +40,7 @@ class Encuesta {
                         isAnonymous = false
                     )
                 }
-
+                //encuesta
                 command("apalancamiento") {
                     bot.sendPoll(
                         chatId = ChatId.fromId(message.chat.id),
@@ -51,6 +51,7 @@ class Encuesta {
                         isAnonymous = false
                     )
                 }
+                //encuesta
                 command("criptomoneda") {
                     bot.sendPoll(
                         chatId = ChatId.fromId(message.chat.id),
